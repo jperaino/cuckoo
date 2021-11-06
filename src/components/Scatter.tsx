509bc0e2fe44d8a9a76682ee40cccad2052625bc
jperaino <jprn72@gmail.com>
2021-11-06 17:41:43 -0400

    send list of selected to flask

--- src/components/Scatter.tsx
+++ src/components/Scatter.tsx
@@ -1,68 +1,100 @@
 import { Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
-import {thisData} from "../data.js"
+import { thisData } from "../data.js";
+import $ from "jquery";
 
 const Scatter = () => {
-  
-  const [formattedData, setFormattedData] = useState<any>([])
-  const [selectedId, setSelectedId] = useState<string | number>('none')
- 
+  const [formattedData, setFormattedData] = useState<any>([]);
+  const [selectedIds, setSelectedIds] = useState<string[] | number[]>([]);
+
+  const [xVar, setXVar] = useState<string>("Area");
+  const [yVar, setYVar] = useState<string>("Volume");
+
   useEffect(() => {
-    parseData()
-  }, [])
+    parseData();
+  }, []);
 
-const onClick = (inputData: any) => {
-  const { points } = inputData;
-  const { data, pointNumber, pointIndex } = points[0];
-  const { x, y, labels } = data;
+  const getIdsFromSelection = (inputData: any): any[] => {
+    const { points } = inputData;
+    var ids: any[] = [];
 
-  const selectedX = x[pointIndex];
-  const selectedY = y[pointIndex];
-  const selectedLabel = labels[pointIndex];
+    for (let i = 0; i < points.length; i++) {
+      const { data, pointIndex } = points[i];
+      const { labels } = data;
 
-  console.log(`Revit ID: ${selectedLabel}`);
-  setSelectedId(selectedLabel)
-};
-  
+      const label = labels[pointIndex];
+      ids.push(label);
+    }
+
+    console.log(ids);
+    return ids;
+  };
+
+  const sendIdsToFlask = (ids: any[]) => {
+    console.log("Sending ids to flask");
+
+    // Convert to JSON
+    const json = JSON.stringify({ ids: ids });
+
+    // Transmit
+    $.ajax({
+      type: "POST",
+      url: "localhost:5000/storeSelected",
+      data: json,
+    });
+  };
+
+  const handleSelection = (inputData: any) => {
+    const ids = getIdsFromSelection(inputData);
+    sendIdsToFlask(ids);
+  };
+
+  const onClick = (inputData: any) => {
+    handleSelection(inputData);
+  };
+
+  const onSelected = (inputData: any) => {
+    handleSelection(inputData);
+  };
+
   const parseData = () => {
-    var xData: number[] = []
-    var yData: number[] = []
-    var labelsData: number[] = []
+    var xData: number[] = [];
+    var yData: number[] = [];
+    var labelsData: number[] = [];
 
     thisData.forEach((row) => {
-      xData.push(row.Area);
-      yData.push(row.Volume);
+      xData.push(row[xVar]);
+      yData.push(row[yVar]);
       labelsData.push(row.ID);
-    })
-  
-    setFormattedData(
-      [
-        {
-          x: xData, 
-          y: yData,
-          labels: labelsData, 
-          type: "scatter",
-          mode: "markers",
-          marker: { color: "red" }
-        }
-      ]
-    )
-  }
+    });
+
+    setFormattedData([
+      {
+        x: xData,
+        y: yData,
+        labels: labelsData,
+        type: "scatter",
+        mode: "markers",
+        marker: { color: "red" },
+      },
+    ]);
+  };
 
   return (
     <Grid container direction="column">
       <Grid item>
-        <Typography variant="h3">Walls</Typography>
+        <Typography variant="h3">Cuckoo</Typography>
       </Grid>
-      <Grid item>
+      {/* <Grid item>
         <Typography variant="h4">{`Selected wall id: ${selectedId}`}</Typography>
-      </Grid>
+      </Grid> */}
       <Grid item>
         <Plot
           data={formattedData}
-          layout={{ width: 800, height: 800 }}
+          layout={{ width: 1000, height: 600 }}
           onClick={onClick}
+          onSelected={onSelected}
         />
       </Grid>
     </Grid>
