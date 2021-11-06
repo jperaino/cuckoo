c4479d067ebb88e71e7a5f850fb1c9918b42de43
Jim Peraino <jprn72@gmail.com>
2021-11-06 15:13:14 -0400

    add scatter

--- src/components/Scatter.tsx
+++ src/components/Scatter.tsx
@@ -1,10 +1,18 @@
 import { Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
+import {thisData} from "../data.js"
+
+const Scatter = () => {
+  
+  const [formattedData, setFormattedData] = useState<any>([])
+  const [selectedId, setSelectedId] = useState<string | number>('none')
+ 
+  useEffect(() => {
+    parseData()
+  }, [])
 
 const onClick = (inputData: any) => {
-  //   console.log(data);
-  //   console.log(data?.points);
   const { points } = inputData;
   const { data, pointNumber, pointIndex } = points[0];
   const { x, y, labels } = data;
@@ -13,44 +21,48 @@ const onClick = (inputData: any) => {
   const selectedY = y[pointIndex];
   const selectedLabel = labels[pointIndex];
 
-  console.log(selectedX, selectedY, selectedLabel);
+  console.log(`Revit ID: ${selectedLabel}`);
+  setSelectedId(selectedLabel)
 };
+  
+  const parseData = () => {
+    var xData: number[] = []
+    var yData: number[] = []
+    var labelsData: number[] = []
 
-const Scatter = () => {
+    thisData.forEach((row) => {
+      xData.push(row.Area);
+      yData.push(row.Volume);
+      labelsData.push(row.ID);
+    })
+  
+    setFormattedData(
+      [
+        {
+          x: xData, 
+          y: yData,
+          labels: labelsData, 
+          type: "scatter",
+          mode: "markers",
+          marker: { color: "red" }
+        }
+      ]
+    )
+  }
+
   return (
     <Grid container direction="column">
       <Grid item>
         <Typography variant="h3">Walls</Typography>
       </Grid>
       <Grid item>
+        <Typography variant="h4">{`Selected wall id: ${selectedId}`}</Typography>
+      </Grid>
+      <Grid item>
         <Plot
-          data={[
-            {
-              x: [1, 2, 3, 8, 9, 1, 4, 1, 5, 5, 7, 3],
-              y: [2, 6, 3, 1, 2, 4, 5, 2, 4, 2, 2, 3],
-              labels: [
-                "a",
-                "b",
-                "c",
-                "a",
-                "b",
-                "c",
-                "a",
-                "b",
-                "c",
-                "a",
-                "b",
-                "c",
-              ],
-              type: "scatter",
-              mode: "markers",
-              marker: { color: "red" },
-            },
-            // { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
-          ]}
+          data={formattedData}
           layout={{ width: 800, height: 800 }}
           onClick={onClick}
-          //   onSelected={(data) => console.log(data)}
         />
       </Grid>
     </Grid>
